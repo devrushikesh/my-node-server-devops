0c2a3c936f786e2418617cc7a755666d425e8119
Rushikesh Choudhari <165548239+devrushikesh@users.noreply.github.com>
2025-12-01 20:34:26 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, Rushikesh Choudhari!");
+  res.send("Hello, Rushikesh Navnath Choudhari!");
 });
 
 app.listen(PORT, () => {
